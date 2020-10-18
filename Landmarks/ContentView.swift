a34c99c38504e6279cde926fdb483cd1eaadbd6f
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2020-10-18 11:52:18 +0700

    Initial commit with the code
    Chapter 1 is done

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Pavel Ditenbir on 18.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
